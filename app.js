760720994e213d0d719e6c15353b1ad80eeca066
IngieoAndSpare <wjh4877@naver.com>
2019-01-02 13:42:22 +0900

    backup

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ var coordinateX,
     zoomLevel,
     apiKey = '';
 
+// menu text 
+const menuText = [
+    '지도 종류를 선택해 주세요. (1. vworld, 2. naver, 3. duam, 4. google)',
+    'x 좌표를 입력해 주세요.',
+    'y 좌표를 입력해 주세요.',
+    'zoom level을 입력해 주세요.',
+    'API key를 입력해 주세요.'
+]
+
 // url context
 var vworldUrlContext = '',
     naverUrlContext = '',
@@ -22,6 +31,9 @@ const mapTileSavePathContext = '';
 var readLineInterface = readLine.createInterface(process.sdtin, process.stdout, null);
 
 module.exports = {
+    initAppHandler : function () {
+        //TODO : app init 
+    },
     doRequestMapTile : function (url) {
         //TODO : map tile request
         axios.get(url).then(response => {
@@ -32,10 +44,10 @@ module.exports = {
     },
     getUserInput : function () {
         //TODO : coordinate request, map vender request
+
     },
     validation : function (inputValue, vender) {
         //TODO : validation user input value
 
     },
-
 }
