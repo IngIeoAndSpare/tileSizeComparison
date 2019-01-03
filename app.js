e4a092f9b5db0c8521c3059a9c104c83ee33d680
WI-Hyun-joong <wjh4877@naver.com>
2019-01-03 16:14:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,14 @@
 var fs = require('fs'),
 	readLineSync = require('readline-sync'),
 	axios = require('axios'),
-    path = require('path');
+    checkSize = require('get-folder-size');
 
 // user input value 16x8 size request
-var userInputData = []; // 0. vender 1. coordinateX, 2. coordinateY, 3. zoomLevel, 4. apiKey(option)
-var saveFlag = false;
+var userInputData = []; // 0. vender 1. coordinateX, 2. coordinateY, 3. zoomLevel, 4. location name ,5. apiKey(option)
 
 // menu text 
 const menuText = [
-    '지도 종류를 선택해 주세요. (1. vworld, 2. naver, 3. duam, 4. google) => ',
+    '지도 종류를 선택해 주세요. (1. vworld, 2. naver, 3. duam, 4. google, 5. vwolrd(2018ver)) => ',
     'x 좌표를 입력해 주세요. => ',
     'y 좌표를 입력해 주세요. => ',
     'zoom level을 입력해 주세요. => ',
@@ -24,27 +23,30 @@ const subMenu = [
 	'설악산 (x: y: z:), 독도(x: y: z:), 대전 정부청사(x: y: z:)'
 ]
 const mapVender = [
-	'vworld', 'naver', 'daum', 'google'
+	'vworld', 'naver', 'daum', 'google', 'vworld2018'
 ]
-
-
 // url context
-var vworldUrlContext = '',
+var vworldUrlContext = 'http://xdworld.vworld.kr:8080/XDServer/3DData?Version=2.0.0.0&Request=GetLayer&Layer=tile_mo_HD', //Level=12&IDX=34939&IDY=14471&Key=...',
     naverUrlContext = 'https://simg.pstatic.net/onetile/get', //  197/0/1/11/1750/1223/bl_st_bg (x/y)
     daumUrlContext = 'http://map0.daumcdn.net/map_skyview', //  L6/187/130.jpg?v=160114',
-    googleUrlContext = 'https://khms0.google.co.kr/kh;'  //v=821?x=3499&y=1602&z=12';
+	googleUrlContext = 'https://khms0.google.co.kr/kh',  //v=821?x=3499&y=1602&z=12',
+	vwolrdUrlContext2018 = 'http://xdworld.vworld.kr:8080/XDServer/requestLayerNode?Layer=2018'; //Level=12&IDX=34939&IDY=14471&APIKey=...';
 
 module.exports = {
     initAppHandler : function () {
         const thisApp = this;
-		this.getUserInput();  
+		this.getUserInput();  // input user select
+		// setting coordinate 
 		let coordinateX = Number(userInputData[1]),
 			coordinateY = Number(userInputData[2]),
 			vender = mapVender[Number(userInputData[0]) - 1] + '_' + userInputData[4];
+		// check dir and mkdir
 		this.checkDirectory(vender);
 
+		// request image tile loop 16x8 size
 		for(let x = coordinateX; x < coordinateX + 16; x++) {
 			for(let y = coordinateY; y < coordinateY + 8; y++) {
+				// file metadata setting
 				let tileInfo = {
 					vender,
 					coordinateSet : {
@@ -52,6 +54,7 @@ module.exports = {
 					},
 					zoomLevel : Number(userInputData[3])
 				};
+				// request image tile
 				thisApp.doRequestMapTile(thisApp.setUrl(x, y), tileInfo);
 			}
 		}
@@ -61,30 +64,47 @@ module.exports = {
         axios.get(url, {
 			responseType : 'arraybuffer'
 		}).then(response => {
+			// convert response (image => base 64)
 			let imageResult = new Buffer(response.data, 'binary').toString('base64');
 			thisApp.saveTile(imageResult, tileInfo);
         }).catch(err => {
+			console.log('get tile data err =>  x: ' + tileInfo.coordinateSet.x + '  y: ' + tileInfo.coordinateSet.y + '  zoom: ' + tileInfo.zoomLevel);
 			console.log(err);
-			return null;
         });
     },
     saveTile : function (data, tileInfo) {
+		const thisApp = this;
 		let imageName = tileInfo.coordinateSet.x + '_' + tileInfo.coordinateSet.y + '_' + tileInfo.zoomLevel + '.jpg',
 			folderPath = './' + tileInfo.vender;
 
+		//create tile image file
 		fs.writeFile(folderPath + '/' + imageName , data, {encoding : 'base64'}, function (err) {
 			if(!err) {
 				console.log(imageName + ' fileCreate');
+				// add file size
+				thisApp.addFileSize(folderPath + '/' + imageName);
 			} else {
 				console.log(err);
 			}
 		});
 	},
 	checkDirectory : function (vender) {
+		// check directory and mkdir
 		let mapTileSavePath = './' + vender;
 		if(!fs.existsSync(mapTileSavePath)) {
 			fs.mkdirSync(mapTileSavePath);
 		}
+	},
+	addFileSize : function (path) {
+		checkSize(path, (err, size) => {
+			if(err) {
+				console.log(path + 'file read size err. check file');
+				console.log(err);
+			} else {
+				let fileSize = (size/2014).toFixed(2);
+				console.log(fileSize + 'KB');
+			}
+		});
 	},
     getUserInput : function () {
 		for(let consoleLine of menuText) {
@@ -92,20 +112,22 @@ module.exports = {
 		}
 	},
     setUrl : function (x, y) {
-		let urlArray = [vworldUrlContext, naverUrlContext, daumUrlContext, googleUrlContext];
 		let url;
 		switch(Number(userInputData[0])) {
 			case 1 :
-				url = urlArray[0]
+				url = vworldUrlContext + '&Level=' + Number(userInputData[3]) + '&IDX=' + x + '&IDY=' + y + '&Key=' + userInputData[5] ;
 				break;
 			case 2 :
-				url = urlArray[1] + '/197/0/1/' + Number(userInputData[3]) + '/' + x + '/' + y + '/bl_st_bg';;
+				url = naverUrlContext + '/197/0/1/' + Number(userInputData[3]) + '/' + x + '/' + y + '/bl_st_bg';;
 				break;
 			case 3 :
-				url = urlArray[2] + '/L' + Number(userInputData[3]) + '/' + y + '/' + x + '.jpg?v=160114';
+				url = daumUrlContext + '/L' + Number(userInputData[3]) + '/' + y + '/' + x + '.jpg?v=160114';
 				break;
 			case 4 :
-				url = urlArray[3] + '/v=821?x=' + x + '&y=' + y + '&z=' + Number(userInputData[3]);
+				url = googleUrlContext + '/v=821?x=' + x + '&y=' + y + '&z=' + Number(userInputData[3]);
+				break;
+			case 5 :
+				url = vwolrdUrlContext2018 + '&Level=' + Number(userInputData[3]) + '&IDX=' + x + '&IDY=' + y + '&APIKey=' + userInputData[5] ;;
 				break;
 		}
 		return url;
